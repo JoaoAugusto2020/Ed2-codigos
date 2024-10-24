4081d78a553ab82edbc57cc7568976800c794ac7
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2024-10-24 17:11:03 -0300

    Add files via upload

--- atv2-classroom/exercicio1.cpp
+++ atv2-classroom/exercicio1.cpp
@@ -3,144 +3,105 @@
 #include<string.h>
 #include<fstream>
 
-#define LIMITE 1000
+#define LIMITE 10
 
 using namespace std;
 
-struct No {
-    char nome[100];
-    No* prox;
-
-    No() {
-        prox = NULL;
-    }
-
-};
-
-struct Lista {
-    No* inicio;
-    No* fim;
-    int n;
-
-    Lista() {
-        inicio = NULL;
-        fim = NULL;
-        n = 0;
-    }
-
-    void inserirFinal(char nome[]) {
-    	
-        No* novo = new No();
-        strcpy(novo->nome, nome);
-		if (inicio == NULL) {
-            inicio = novo;
-            fim = novo;
-        } else {
-            fim->prox = novo;
-            fim = novo;
-        }
-        n++;
-        
-		//cout<<n<<": "<<novo->nome<<endl;
-    }
-	
-	void imprimir() {
-        No* aux = inicio;
-        
-        //printf("%d %d\n", aux, aux->valor);
-        while (aux != NULL) {
-            cout<<aux->nome<<endl;
-            //printf("%d %d\n", aux->prox, aux->nome);
-            aux = aux->prox;
-        }
-    }
-};
-
-
-void troca(Lista nomes, int a, int b){
-    Lista aux = v[a];
-    v[a] = v[b];
-    v[b] = aux;
+void troca(char nomes[LIMITE][100], int a, int b){
+    char aux[100];
+	strcpy(aux, nomes[a]);
+	strcpy(nomes[a], nomes[b]);
+	strcpy(nomes[b], aux);
 }
 
 //complexidade de espaço: O(1) (constante)
 //complexidade de tempo: O(n²) (exponencial)
-void bubbleSortOtimizado(Lista nomes, int n){
+void bubbleSortOtimizado(char nomes[LIMITE][100], int n){
     for(int i=0; i < n-1; i++){
     	bool houveTroca = false;
+    	
         for(int j=0; j < (n-1) - i; j++){
-            if(v[j] > v[j+1]){
-                troca(v, j, j+1);
+			if(strcmp(nomes[j],nomes[j+1])>0){
+                troca(nomes, j, j+1);
                 houveTroca = true;
             }
         }
-        if(!houveTroca) return;
+        
+        if(houveTroca==false) return;
     }
 }
 
 //complexidade de espaço: O(1) (constante)
 //complexidade de tempo: O(n²) (exponencial)
-void selectionSort(Lista nomes, int n){
+void selectionSort(char nomes[LIMITE][100], int n){
 	for(int i=0; i < n-1; i++){
 		for(int j=i+1; j < n; j++){
-			if(v[i] > v[j]){
-				troca(v, i, j);
+			if(strcmp(nomes[i],nomes[j])>0){
+				troca(nomes, i, j);
 			}
 		}
 	}
 }
 
-//complexidade de espaço: O(1) (constante)
-//complexidade de tempo: O(n²) (exponencial)
-void insertionSort(Lista nomes, int n){
-	for(int i=1; i < n; i++){
-		vector<string> aux = v[i];
+////complexidade de espaço: O(1) (constante)
+////complexidade de tempo: O(n²) (exponencial)
+void insertionSort(char nomes[LIMITE][100], int n){
+	for(int i=1; i<n; i++){
+		char aux[100];
+		strcpy(aux, nomes[i]);
 		int j = i-1;
 		
-		while(j>=0 && aux<v[j]){
-			v[j + 1] = v[j];
+		while(j>=0 && strcmp(aux, nomes[j])<0){
+			strcpy(nomes[j+1], nomes[j]);
 			j--;
 		}
 		
-		v[j+1] = aux;
+		strcpy(nomes[j+1], aux);
 	}
 }
 
 // Complexidade de tempo e espaço = O(n)
-void merge(Lista nomes, int ini, int meio, int fim) {
-    vector<string> tmp[(fim-ini) + 1];
+void merge(char nomes[LIMITE][100], int ini, int meio, int fim) {
+    char tmp[(fim-ini) + 1][100];
     int i=ini;
 	int j=meio + 1;
 	int k=0;
     
     //meio e fim são os limites dos vetores divididos
     while (i<=meio && j<=fim) {
-        tmp[k++] = (v[i] < v[j]) ? v[i++] : v[j++];
+        if(strcmp(nomes[i], nomes[j])<0){
+			strcpy(tmp[k], nomes[i]);
+        	i++;
+		}else{
+			strcpy(tmp[k], nomes[j]);
+			j++;
+		}
+		k++;
     }
     
     //restos
     while (i<=meio) {
-        tmp[k++] = v[i++];
+        strcpy(tmp[k++], nomes[i++]);
     }
     while (j<=fim) {
-        tmp[k++] = v[j++];
+        strcpy(tmp[k++], nomes[j++]);
     }
     
     //transferindo pra original
     for (i=ini, k=0; i<=fim; i++, k++) {
-        v[i] = tmp[k];
+        strcpy(nomes[i], tmp[k]);
     }
 }
 
 //complexidade de espaço: O(n) (linear)
 //complexidade de tempo: O(n log n)
-void mergeSort(Lista nomes, int ini, int fim) {
+void mergeSort(char nomes[LIMITE][100], int ini, int fim) {
     if (ini < fim) {
         int meio = (ini + fim) / 2;
         //printf("%d %d %d\n", ini, meio, fim);
-        mergeSort(v, ini, meio);
-        mergeSort(v, meio + 1, fim);
-        merge(v, ini, meio, fim);
+        mergeSort(nomes, ini, meio);
+        mergeSort(nomes, meio + 1, fim);
+        merge(nomes, ini, meio, fim);
     }
 }
 
@@ -148,7 +109,8 @@ int main(){
 	int escolha=0;
 	
 	while(escolha!=5){
-	    Lista nomes;
+		int n=LIMITE;
+	    char nomes[LIMITE][100];
 	    string linha;
 	
 		//ifstream – abre o arquivo apenas para leitura
@@ -156,14 +118,15 @@ int main(){
 		
 		if (arq.is_open()) {
 			//eof() - retorna true ao atingir o fim do arquivo
-			int i=1;
+			int i=0;
 			while( !arq.eof() ){
 				if(i>LIMITE) break;
 				
 				getline(arq, linha);
-			    char nome[100]; 
-			    strcpy(nome, linha.c_str());
-				nomes.inserirFinal(nome);
+
+				char nome[100]; 
+			    strcpy(nomes[i], linha.c_str());
+
 				i++;
 			}
 			
@@ -171,10 +134,13 @@ int main(){
 		}else{
 			cout<<"ERRO: arquivo nao foi aberto ou nao existe"<<endl;
 		}
-	    
-		nomes.imprimir();
-		cout<<nomes.n<<endl;
 		
+		cout<<"ANTES:"<<endl;
+		for(int i=0; i<n; i++){
+			cout<<"Linha "<<i<<": "<<nomes[i]<<endl;
+		}
+		cout<<"\n";
+	    
 		cout<<"Escolha um dos algoritmos de ordenacao abaixo:"<<endl;
 		cout<<"1 - BubbleSort"<<endl;
 		cout<<"2 - SelectionSort"<<endl;
@@ -196,13 +162,19 @@ int main(){
 			return 0;
 		}else{
 			cout<<"Opcao invalida! Escolha novamente!"<<endl;
-			cout<<endl;
+			cout<<"\n";
 			
-			system("pause");
 			continue;
 		}
 		
+		cout<<"DEPOIS:"<<endl;
+		for(int i=0; i<n; i++){
+			cout<<"Linha "<<i<<": "<<nomes[i]<<endl;
+		}
+		
 		system("pause");
+		system("cls");
+		fflush(stdin);
 	}
     
 	return 0;
